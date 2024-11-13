6c0f05ae522449bead2f2f58456b34fcd0fc7c5b
Bruce-Lutaj <lutajbrus@gmail.com>
2024-11-13 16:13:24 +0100

    nov 13 work

--- algo/calculations.c
+++ algo/calculations.c
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   movement.c                                         :+:      :+:    :+:   */
+/*   calculations.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brulutaj <brulutaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 23:20:33 by chsassi           #+#    #+#             */
-/*   Updated: 2024/11/13 09:48:18 by brulutaj         ###   ########.fr       */
+/*   Created: 2024/11/12 12:11:26 by brulutaj          #+#    #+#             */
+/*   Updated: 2024/11/12 12:11:31 by brulutaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-int	rotate_player(t_all *cubed, int i)
-{
-	if (i == 0)
-	{
-		cubed->player.angle -= ROTATION_SPEED;
-		if (cubed->player.angle < 0)
-			cubed->player.angle += 2 * PI;
-	}
-	else
-	{
-		cubed->player.angle += ROTATION_SPEED;
-		if (cubed->player.angle > 2 * PI)
-			cubed->player.angle -= 2 * PI;
-	}
-	return (0);
-}
 
--- cub3d.h
+++ cub3d.h
@@ -17,6 +17,7 @@
 # include <stdbool.h>
 # include "libft/headers/libft.h"
 
+# define PI 3.14159265358979323846
 # define SCREEN_W 720
 # define SCREEN_H 1080
 # define ROTATION_SPEED 0.050

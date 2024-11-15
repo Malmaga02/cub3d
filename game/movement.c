16a47fc096d30efff447bb2b3c8b45ea9d5cead7
Bruce-Lutaj <lutajbrus@gmail.com>
2024-11-15 13:59:19 +0100

    Errors in rotation solved

--- game/movement.c
+++ game/movement.c
@@ -6,13 +6,13 @@
 /*   By: brulutaj <brulutaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 23:20:33 by chsassi           #+#    #+#             */
-/*   Updated: 2024/11/14 17:49:57 by brulutaj         ###   ########.fr       */
+/*   Updated: 2024/11/15 13:56:28 by brulutaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	rot_action(t_all *cubed, int old_dirx, int old_planex, int rot)
+int	rot_action(t_all *cubed, int old_dirx, int old_planex, double rot)
 {
 	old_dirx = cubed->player.dir.x;
 	cubed->player.dir.x = cubed->player.dir.x * cos(rot)
@@ -32,9 +32,11 @@ int	rotate_player(t_all *cubed)
 	double	old_dirx;
 	double	old_planex;
 
-	if (cubed->event.rotate_right)
+	old_dirx = cubed->player.dir.x;
+	old_planex = cubed->player.plane.x;
+	if (cubed->event.rotate_right == true)
 		rot_action(cubed, old_dirx, old_planex, -ROT);
-	if (cubed->event.rotate_left)
+	if (cubed->event.rotate_left == true)
 		rot_action(cubed, old_dirx, old_planex, ROT);
 	return (0);
 }
